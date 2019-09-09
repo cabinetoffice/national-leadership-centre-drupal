6c1e411635351cea51e8aa73ad315445e42b3af7
Mike Bell <hello@mikebell.io>
2019-09-09 16:34:29 +0100

    Do not commit local DB settings to production!!!

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -785,13 +785,3 @@ if (getenv('NLC_ENVIRONMENT')) {
     include __DIR__ . '/settings.' . getenv('NLC_ENVIRONMENT') . '.php';
   }
 }
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'mariadb',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
