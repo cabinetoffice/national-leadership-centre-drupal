faa5da794fbc925400b3bc09cd4720c11b6870a4
Joe Baker <joesbaker@gmail.com>
2019-12-03 17:51:08 +0000

    Do GA copy checking in a JS file instead of dropped into config [ch180]

---
+++ web/modules/custom/nlc_prototype/js/nlc_prototype.google_analytics.js
@@ -0,0 +1,20 @@
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.nlc_prototype_ga = {
+    attach: function(context, settings) {
+      $("a[href^='tel:'],a[href^='mailto:']").bind({
+        copy: function() {
+          const value = this.href.includes('tel:') ? this.href.substring(4) : this.href.substring(7);
+          gtag('event', 'Connection', {
+            event_category: this.href.includes('tel:') ? 'tel' : 'mailto',
+            event_label: $("#block-bevan-page-title h1").text() + ' | ' + $(this).siblings('strong').text(),
+            value: value,
+            transport_type: 'beacon'
+          });
+        }
+      });
+    }
+  }
+
+})(jQuery, Drupal, drupalSettings);
