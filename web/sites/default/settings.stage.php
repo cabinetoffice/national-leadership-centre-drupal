17ed45d40be3379a0bd027b25d61621b3aa9e009
Joe Baker <joesbaker@gmail.com>
2019-12-20 10:27:12 +0000

    Set s3fs.upload_as_private as true in prod and stage — now validates [ch1139]

--- web/sites/default/settings.stage.php
+++ web/sites/default/settings.stage.php
@@ -23,6 +23,7 @@ $settings['s3fs.bucket'] = $s3bucketCreds['bucket_name'];
 $settings['s3fs.region'] = $s3bucketCreds['aws_region'];
 $config['s3fs.settings']['bucket'] = $s3bucketCreds['bucket_name'];
 $settings['s3fs.use_s3_for_public'] = TRUE;
+$settings['s3fs.upload_as_private'] = TRUE;
 
 // Sendgrid integration API key
 $config['sendgrid_integration.settings']['apikey'] = getenv('SG_API_KEY');
