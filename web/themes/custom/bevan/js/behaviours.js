42a3a58e372858fb0e5acfcb5e0f12e3428bcac2
Martin Fraser <martin@darksnow.net>
2020-01-31 16:42:57 +0000

    Remember click events and reset focus

--- web/themes/custom/bevan/js/behaviours.js
+++ web/themes/custom/bevan/js/behaviours.js
@@ -2,13 +2,30 @@
   'use strict';
   
   Drupal.behaviors.bevantheme = {
+    focusFacetId: null,
     attach: function (context, settings) {
       $('.eu-cookie-compliance__close').click(function() {
         Drupal.eu_cookie_compliance.toggleWithdrawBanner();
       });
 
-      // Once the facet summary block has loaded.
+      // Get the result count once the facet results have loaded.
       $('#result-count').text($('#facet-summary-wrapper').data('facets-summary-count') + ' results');
+
+      // Track facet clicks so we can restore focus.
+      $('.facet-item').click(function(ev) {
+        Drupal.behaviors.bevantheme.focusFacetId = $(ev.target).data('drupal-facet-item-id');
+      });
+
+      // The summary block loads last so if it's the current AJAX context the check boxes exist.
+      // If we set the focus earlier, it gets reset on the next load.
+      if (Drupal.behaviors.bevantheme.focusFacetId && $(context).data('drupal-facets-summary-id') == 'facet_summary') {
+        var el = document.querySelector('[data-drupal-facet-item-id="' + Drupal.behaviors.bevantheme.focusFacetId + '"]');
+        if (el) {
+          el.focus();
+          // Clean up so we don't trigger this in unexpected AJAX calls.
+          Drupal.behaviors.bevantheme.focusFacetId = null;
+        }
+      }
     }
   };
 })(jQuery, Drupal, drupalSettings);
