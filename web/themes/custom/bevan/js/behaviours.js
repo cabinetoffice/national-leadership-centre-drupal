2f8fbe58fdf5e01f0df53ec938228b4e7e190397
Martin Fraser <martin@darksnow.net>
2020-02-11 11:12:30 +0000

    Only set the count if it actually changes

--- web/themes/custom/bevan/js/behaviours.js
+++ web/themes/custom/bevan/js/behaviours.js
@@ -8,9 +8,6 @@
         Drupal.eu_cookie_compliance.toggleWithdrawBanner();
       });
 
-      // Get the result count once the facet results have loaded.
-      $('#result-count').text($('#facet-summary-wrapper').data('facets-summary-count') + ' results');
-
       // Track facet clicks so we can restore focus.
       $('.facet-item a').click(function(ev) {
         Drupal.behaviors.bevantheme.focusFacetId = $(ev.target).data('drupal-facet-item-id');
@@ -18,12 +15,21 @@
 
       // The summary block loads last so if it's the current AJAX context the check boxes exist.
       // If we set the focus earlier, it gets reset on the next load.
-      if (Drupal.behaviors.bevantheme.focusFacetId && $(context).data('drupal-facets-summary-id') == 'facet_summary') {
-        var el = document.querySelector('[data-drupal-facet-item-id="' + Drupal.behaviors.bevantheme.focusFacetId + '"]');
-        if (el) {
-          el.focus();
-          // Clean up so we don't trigger this in unexpected AJAX calls.
-          Drupal.behaviors.bevantheme.focusFacetId = null;
+      if ($(context).data('drupal-facets-summary-id') == 'facet_summary') {
+
+        // Only update the count if the text has actually changed, prevent screeen readers reading it twice.
+        var countText = $('#facet-summary-wrapper').data('facets-summary-count') + ' results';
+        if (countText != $('#result-count').text()) {
+          $('#result-count').text(countText);
+        }
+
+        if (Drupal.behaviors.bevantheme.focusFacetId) {
+          var el = document.querySelector('[data-drupal-facet-item-id="' + Drupal.behaviors.bevantheme.focusFacetId + '"]');
+          if (el) {
+            el.focus();
+            // Clean up so we don't trigger this in unexpected AJAX calls.
+            Drupal.behaviors.bevantheme.focusFacetId = null;
+          }
         }
       }
     }
