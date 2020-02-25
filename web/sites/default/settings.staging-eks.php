190577bff780bcb1eb661d8efb5890be3b5dc4cd
Joe Baker <joesbaker@gmail.com>
2020-02-25 14:27:47 +0000

    Catch typo [ch2595]

--- web/sites/default/settings.staging-eks.php
+++ web/sites/default/settings.staging-eks.php
@@ -33,7 +33,7 @@ $config['sendgrid_integration.settings']['apikey'] = getenv('SENDGRID_API_KEY');
 $config['config_split.config_split.devel']['status'] = FALSE;
 // Ensure the correct CRM config environment is active
 $config['config_split.config_split.crm']['status'] = FALSE;
-< $config['config_split.config_split.crm_prod']['status'] = FALSE;
+$config['config_split.config_split.crm_prod']['status'] = FALSE;
 $config['config_split.config_split.crm_stage']['status'] = TRUE;
 
 if (getenv('CONNECT_DOMAIN')) {
