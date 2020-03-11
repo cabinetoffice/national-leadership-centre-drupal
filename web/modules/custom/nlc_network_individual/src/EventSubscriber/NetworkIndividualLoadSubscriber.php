55c34ba5c47c7f295407aff84728b5a182500861
Joe Baker <joesbaker@gmail.com>
2020-03-11 13:46:56 +0000

    Stash some changes

--- web/modules/custom/nlc_network_individual/src/EventSubscriber/NetworkIndividualLoadSubscriber.php
+++ web/modules/custom/nlc_network_individual/src/EventSubscriber/NetworkIndividualLoadSubscriber.php
@@ -3,19 +3,13 @@
 namespace Drupal\nlc_network_individual\EventSubscriber;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Path\CurrentPathStack;
-use Drupal\Core\Path\PathMatcherInterface;
-use Drupal\Core\Routing\RouteBuildEvent;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Routing\RoutingEvents;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\neo4j_db_entity\Event\Neo4jDbEntityEvent;
 use Drupal\neo4j_db_entity\Event\Neo4jDbEntityEventType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\KernelEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 
-class NetworkIndividualViewSubscriber implements EventSubscriberInterface {
+class NetworkIndividualLoadSubscriber implements EventSubscriberInterface {
 
   /**
    * The current user.
@@ -25,57 +19,28 @@ class NetworkIndividualViewSubscriber implements EventSubscriberInterface {
   protected $currentUser;
 
   /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The current route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
    * Constructs a new NetworkIndividualViewSubscriber instance.
    *
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The current route match.
    *
    */
-  public function __construct(AccountInterface $current_user, EntityTypeManagerInterface $entity_type_manager, RouteMatchInterface $route_match) {
+  public function __construct(AccountInterface $current_user) {
     $this->currentUser = $current_user;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->routeMatch = $route_match;
   }
 
   /**
    * @param \Drupal\Core\Routing\RouteBuildEvent $event
    *   The route build event.
    */
-  public function onRequest(KernelEvent $event) {
-//    foreach ($this->entityTypeManager->getDefinitions() as $entity_type) {
-//      if ($entity_type->hasRouteProviders()) {
-//      }
-//    }
+  public function onLoad(Neo4jDbEntityEvent $event) {
 
-    if ($this->routeMatch->getParameter('user')) {
-      /** @var \Drupal\user\UserInterface $user */
-      $user = $this->routeMatch->getParameter('user');
-      \Drupal::logger('nlc_network_individual')->debug($user->id());
-    }
   }
 
   public static function getSubscribedEvents() {
     $events = [];
 
-    $events[Neo4jDbEntityEventType::LOAD][] = ['onView'];
+    $events[Neo4jDbEntityEventType::LOAD][] = ['onLoad'];
 
     return $events;
   }
