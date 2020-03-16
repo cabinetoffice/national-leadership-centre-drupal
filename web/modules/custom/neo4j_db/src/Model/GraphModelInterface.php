c635b99e0a3cfc9575ad79bae1e7269448539f25
Joe Baker <joesbaker@gmail.com>
2020-03-16 17:59:53 +0000

    Not sure yet what is essential to a graph model - will come in due course [ch1232]

--- web/modules/custom/neo4j_db/src/Model/GraphModelInterface.php
+++ web/modules/custom/neo4j_db/src/Model/GraphModelInterface.php
@@ -4,13 +4,4 @@ namespace Drupal\neo4j_db\Model;
 
 interface GraphModelInterface {
 
-  /**
-   * Discover a named model object.
-   *
-   * @param $name
-   *
-   * @return \Drupal\neo4j_db\Model\ModelInterface
-   */
-  public function discover($name);
-
 }
