a48e04d5632fd8d9403d39790f1f22f297514820
Lewis Nyman <hello@lewisnyman.co.uk>
2020-05-21 14:34:48 +0100

    ch4446: Add a specific event to track library link clicks

--- web/modules/custom/nlc_prototype/js/nlc_prototype.google_analytics.js
+++ web/modules/custom/nlc_prototype/js/nlc_prototype.google_analytics.js
@@ -1,59 +1,94 @@
 (function ($, Drupal, drupalSettings) {
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.nlc_prototype_ga = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
+      // Track copy events on phone and email links
       $("a[href^='tel:'],a[href^='mailto:']").bind({
-        copy: function() {
-          gtag('event', 'Copy', {
-            event_category: this.href.includes('tel:') ? 'Telephone' : 'Mails',
-            event_label: 'UID: ' + drupalSettings.user.uid + ' | Page: ' + drupalSettings.path.currentPath,
-            transport_type: 'beacon'
+        copy: function () {
+          gtag("event", "Copy", {
+            event_category: this.href.includes("tel:") ? "Telephone" : "Mails",
+            event_label:
+              "UID: " +
+              drupalSettings.user.uid +
+              " | Page: " +
+              drupalSettings.path.currentPath,
+            transport_type: "beacon",
           });
-        }
+        },
       });
 
-      $("a[href^='tel:']").bind( {
+      // Track clicks on phone links
+      $("a[href^='tel:']").bind({
         click: function () {
-          gtag('event', 'Click', {
-            event_category: 'Telephone',
-            event_label: 'UID: ' + drupalSettings.user.uid + ' | Page: ' + drupalSettings.path.currentPath,
-            transport_type: 'beacon'
+          gtag("event", "Click", {
+            event_category: "Telephone",
+            event_label:
+              "UID: " +
+              drupalSettings.user.uid +
+              " | Page: " +
+              drupalSettings.path.currentPath,
+            transport_type: "beacon",
           });
-        }
+        },
       });
 
-      $("a[href^='mailto:'],area[href^='mailto:']", context).bind( {
+      // Track clicks on mail links
+      $("a[href^='mailto:'],area[href^='mailto:']", context).bind({
         click: function () {
-          gtag('event', 'Click', {
-            event_category: 'Mails',
-            event_label: 'UID: ' + drupalSettings.user.uid + ' | Page: ' + drupalSettings.path.currentPath + ' | Context: ' + $(this).prev().text(),
-            transport_type: 'beacon'
+          gtag("event", "Click", {
+            event_category: "Mails",
+            event_label:
+              "UID: " +
+              drupalSettings.user.uid +
+              " | Page: " +
+              drupalSettings.path.currentPath +
+              " | Context: " +
+              $(this).prev().text(),
+            transport_type: "beacon",
           });
-        }
+        },
       });
 
-      if ($('.search-no-results', context).length) {
+      // Track instances of no results
+      if ($(".search-no-results", context).length) {
         let query = location.pathname + location.search;
-        gtag('event', 'Search', {
-            event_category: 'ZeroResults',
-            event_label: query,
-            transport_type: 'beacon'
-          }
-        );
+        gtag("event", "Search", {
+          event_category: "ZeroResults",
+          event_label: query,
+          transport_type: "beacon",
+        });
       }
 
-      $('body').once('narcissus').each(function() {
-        if (typeof settings.path.currentPathIsSelfProfile !== 'undefined' && settings.path.currentPathIsSelfProfile === true) {
-          gtag('event', 'PageView', {
-            event_category: 'Own profile page',
-            non_interaction: true,
-            page_location: '/user/XXX',
-            page_title: 'User profile page'
+      // Track when a user views their own profile page
+      $("body")
+        .once("narcissus")
+        .each(function () {
+          if (
+            typeof settings.path.currentPathIsSelfProfile !== "undefined" &&
+            settings.path.currentPathIsSelfProfile === true
+          ) {
+            gtag("event", "PageView", {
+              event_category: "Own profile page",
+              non_interaction: true,
+              page_location: "/user/XXX",
+              page_title: "User profile page",
+            });
+          }
+        });
+
+      $(".connect-library").on("mousedown keyup touchstart", "a", function (
+        event
+      ) {
+        var $link = event.target.closest("a");
+        if ($link.href.match(/^\w+:\/\//i)) {
+          gtag("event", "Click", {
+            event_category: "Library links",
+            event_label: $link.href,
+            transport_type: "beacon",
           });
         }
       });
-    }
+    },
   };
-
 })(jQuery, Drupal, drupalSettings);
