7758ca3616840d9f7fde4b51513409663f4e05ca
Joe Baker <joesbaker@gmail.com>
2020-06-04 18:00:45 +0100

    interface/abstract object for working with Drupal nodes in graph DB [ch1232]

---
+++ web/modules/custom/neo4j_db/modules/neo4j_db_entity/src/Model/Node/GraphEntityNodeModelInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Drupal\neo4j_db_entity\Model\Node;
+
+use Drupal\neo4j_db_entity\Model\GraphEntityModelInterface;
+
+interface GraphEntityNodeModelInterface extends GraphEntityModelInterface {}
